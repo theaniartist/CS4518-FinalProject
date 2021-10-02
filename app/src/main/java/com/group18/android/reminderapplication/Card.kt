23b719196a1329cba19eabe7a6435b3a348286f7
theaniartist <wsly@wpi.edu>
2021-10-02 08:38:17 -0400

    Card fragment shows up

---
+++ app/src/main/java/com/group18/android/reminderapplication/Card.kt
@@ -0,0 +1,6 @@
+package com.group18.android.reminderapplication
+
+import java.util.*
+
+data class Card (val id: UUID = UUID.randomUUID(),
+                 var title: String = "")
---
+++ app/src/main/java/com/group18/android/reminderapplication/CardFragment.kt
@@ -0,0 +1,61 @@
+package com.group18.android.reminderapplication
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.EditText
+import androidx.fragment.app.Fragment
+
+class CardFragment : Fragment() {
+
+    private lateinit var card: Card
+    private lateinit var titleField: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        card = Card()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_card, container, false)
+
+        titleField = view.findViewById(R.id.card_title) as EditText
+
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val titleWatcher = object : TextWatcher {
+            override fun beforeTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                count: Int,
+                after: Int
+            ) {
+                // This space intentionally left blank
+            }
+
+            override fun onTextChanged(
+                sequence: CharSequence?,
+                start: Int,
+                before: Int,
+                count: Int
+            ) {
+                card.title = sequence.toString()
+            }
+
+            override fun afterTextChanged(sequence: Editable?) {
+                // This one too
+            }
+        }
+        titleField.addTextChangedListener(titleWatcher)
+    }
+}
